0603b265e1f2530d26d58e12086168a60ecd351b
maxkonar <kamil_karasiewicz@op.pl>
2024-10-21 22:46:02 +0200

    why choose us section

---
+++ template-parts/why-choose-us.php
@@ -0,0 +1,24 @@
+<?php use App\Helpers; ?>
+
+<div class="why-choose-us">
+    <div class="container">
+        <h2 class="why-choose-us__title"><?php echo esc_html__("Why Choose Us?", "vist"); ?></h2>
+        <div class="why-choose-us__section">
+            <div class="why-choose-us__content">
+                <img class="why-choose-us__image" src="<?php echo Helpers::getImage('person.png'); ?>" alt="icon" width="45" height="100">
+                <p class="why-choose-us__subtitle"><?php echo esc_html__("Experienced Team", "vist"); ?></p>
+                <p class="why-choose-us__details"><?php echo esc_html__("Our team of seasoned professionals brings years of industry experience, ensuring we understand and meet your specific needs.", "vist"); ?></p>
+            </div>
+            <div class="why-choose-us__content">
+                <img class="why-choose-us__image" src="<?php echo Helpers::getImage('star.png'); ?>" alt="icon" width="100" height="100">
+                <p class="why-choose-us__subtitle"><?php echo esc_html__("Experienced Team", "vist"); ?></p>
+                <p class="why-choose-us__details"><?php echo esc_html__("Our team of seasoned professionals brings years of industry experience, ensuring we understand and meet your specific needs.", "vist"); ?></p>
+            </div>
+             <div class="why-choose-us__content">
+                <img class="why-choose-us__image" src="<?php echo Helpers::getImage('thunder.png'); ?>" alt="icon" width="45" height="100">
+                <p class="why-choose-us__subtitle"><?php echo esc_html__("Experienced Team", "vist"); ?></p>
+                <p class="why-choose-us__details"><?php echo esc_html__("Our team of seasoned professionals brings years of industry experience, ensuring we understand and meet your specific needs.", "vist"); ?></p>
+            </div>
+        </div>
+    </div>
+</div>
