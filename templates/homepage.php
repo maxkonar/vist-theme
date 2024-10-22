8d86b853fa491fdc6bb04db34c97e207274d39b4
maxkonar <kamil_karasiewicz@op.pl>
2024-10-22 21:39:59 +0200

    technologies secrion

--- templates/homepage.php
+++ templates/homepage.php
@@ -11,6 +11,8 @@ Template Name: Homepage
     <?php get_template_part('template-parts/who-we-are-section');?>
     <?php get_template_part('template-parts/our-clients-section');?>
     <?php get_template_part('template-parts/homepage-slider');?>
+    <?php get_template_part('template-parts/technologies-we-use');?>
     <?php get_template_part('template-parts/why-choose-us');?>
 </div>
+<div id="contact-us"></div>
 <?php get_footer(); ?>
